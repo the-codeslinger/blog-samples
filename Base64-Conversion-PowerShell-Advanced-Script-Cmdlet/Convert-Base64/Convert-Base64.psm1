c140275660a75ed301209915a2770bd00edf322e
Robert Lohr <4004615+the-codeslinger@users.noreply.github.com>
2024-06-23 17:16:13 +0200

    Add Base64 Conversion PowerShell Advanced Script Cmdlet

---
+++ Base64-Conversion-PowerShell-Advanced-Script-Cmdlet/Convert-Base64/Convert-Base64.psm1
@@ -0,0 +1,116 @@
+function Convert-ToBase64 
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline = $true)]
+        [array] $Data
+    )
+    
+    Process 
+    {
+        if ($null -ne $Data) 
+        {
+            $bytes = [System.Text.Encoding]::UTF8.GetBytes($Data)
+            return [Convert]::ToBase64String($bytes)
+        }
+        else 
+        {
+            Write-Error "No input data provided."
+            return
+        }
+    }
+
+    <#
+    .SYNOPSIS
+
+    Converts data to base64-encoded format.
+
+    .DESCRIPTION
+
+    The Convert-ToBase64 function converts data to base64-encoded format. 
+    The function accepts an array of data as input and returns the base64-encoded string.
+    Pipeline input is supported.
+
+    .PARAMETER Data
+    The data to be converted to base64-encoded format.
+    Can be a string, byte array, or any other data type that can be converted to a byte array.
+
+    .OUTPUTS
+
+    System.String
+
+    .EXAMPLE
+
+    PS> Convert-ToBase64 "Why can't dinosaurs clap their hands? Because they're extinct."
+    V2h5IGNhbid0IGRpbm9zYXVycyBjbGFwIHRoZWlyIGhhbmRzPyBCZWNhdXNlIHRoZXkncmUgZXh0aW5jdC4=
+
+    .EXAMPLE
+
+    PS> Convert-ToBase64 -Data "I'm afraid for the calendar. Its days are numbered."     
+    SSdtIGFmcmFpZCBmb3IgdGhlIGNhbGVuZGFyLiBJdHMgZGF5cyBhcmUgbnVtYmVyZWQu
+
+    .EXAMPLE
+
+    PS> "I was going to try an all almond diet, but that's just nuts." | Convert-ToBase64
+    SSB3YXMgZ29pbmcgdG8gdHJ5IGFuIGFsbCBhbG1vbmQgZGlldCwgYnV0IHRoYXQncyBqdXN0IG51dHMu
+
+    .LINK
+    https://the-codeslinger.com
+    #>
+}
+
+# An advanced function that converts base64-encoded data to its original form.
+function Convert-FromBase64 
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [string] $Base64
+    )
+    
+    Process 
+    {
+        $bytes = [Convert]::FromBase64String($Base64)
+        return [System.Text.Encoding]::UTF8.GetString($bytes)
+    }
+
+    <#
+    .SYNOPSIS
+
+    Converts base64-encoded data to its original form.
+
+    .DESCRIPTION
+
+    The Convert-FromBase64 function converts base64-encoded data to its original form.
+    The function accepts a base64-encoded string as input and returns the original data as a string.
+    Pipeline input is supported.
+
+    .PARAMETER Base64
+
+    The base64-encoded data to be converted to its original form.
+
+    .OUTPUTS
+
+    System.String
+
+    .EXAMPLE
+
+    PS> Convert-FromBase64 "V2hhdCBkbyB5b3UgY2FsbCBhIGZhY3RvcnkgdGhhdCBtYWtlcyBva2F5IHByb2R1Y3RzPyBBIHNhdGlzZmFjdG9yeS4="
+    What do you call a factory that makes okay products? A satisfactory.
+
+    .EXAMPLE
+
+    PS> Convert-FromBase64 -Base64 "SSBvbmx5IGtub3cgMjUgbGV0dGVycyBvZiB0aGUgYWxwaGFiZXQuIEkgZG9uJ3Qga25vdyB5Lg=="
+    I only know 25 letters of the alphabet. I don't know y.
+
+    .EXAMPLE
+
+    "SSBtYWRlIGEgcGVuY2lsIHdpdGggdHdvIGVyYXNlcnMuIEl0IHdhcyBwb2ludGxlc3Mu" | Convert-FromBase64              
+    I made a pencil with two erasers. It was pointless.
+
+    .LINK
+    https://the-codeslinger.com
+    #>
+}
+
+Export-ModuleMember -Function Convert-ToBase64, Convert-FromBase64
---
+++ Base64-Conversion-PowerShell-Advanced-Script-Cmdlet/README.md
@@ -0,0 +1,8 @@
+Base64 Conversion PowerShell Advanced Script Cmdlet
+===================================================
+
+This example shows how to create advanced PowerShell functions that behave like a Cmdlet.
+The samples implement conversion functions for the Base64 encoding scheme.
+Both functions can be used standalone or in a pipeline.
+
+[Read the blog post for details.](TODO)
